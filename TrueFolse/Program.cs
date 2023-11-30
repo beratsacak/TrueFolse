f15020cd0851f20a001634d77225d9926ad5d959
berat584 <berat584@BPC08>
2023-11-30 12:03:38 +0300

    Proje dosyası ekle.

---
+++ TrueFolse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFolse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiTekmiCiftMi sayibul = new SayiTekmiCiftMi();
+            bool sonuc = sayibul.TekCift(1);
+            if (sonuc)
+            {
+                Console.WriteLine("Sayı tektir");
+            }
+            else
+            {
+                Console.WriteLine("sayı çifttir");
+            }
+        }
+    }
+    public class SayiTekmiCiftMi
+    {
+        public bool TekCift(int sayi)
+        {
+            if (sayi % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
